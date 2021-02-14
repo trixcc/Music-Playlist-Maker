7f2a702d4c5cc0a8128cc2d5a23842b1c90ccd93
trixiec <trixiec@students.cs.ubc.ca>
2021-02-14 12:20:40 -0800

    implemented up to SongOptions

--- src/main/ui/PlaylistApp.java
+++ src/main/ui/PlaylistApp.java
@@ -18,22 +18,23 @@ public class PlaylistApp {
     // EFFECTS: processes user input
     private void runPlaylistApp() {
         boolean runOn = true;
-        int command = 0;
+        String command = null;
         input = new Scanner(System.in);
 
         while (runOn) {
             mainMenu();
-            command = input.nextInt();
+            command = input.next();
 
-            if (command == 2) {
+            if (command.equals("2")) {
                 runOn = false;
                 System.out.println("See you later!");
-            } else if (command == 1) {
+            } else if (command.equals("1")) {
                 runOn = false;
                 beginPlaylistMaking();
             } else {
-                System.out.println("Selection not valid.");
+                System.out.println("\nSelection not valid.");
             }
+
         }
     }
 
@@ -69,24 +70,24 @@ public class PlaylistApp {
     //          asks user if they want to add a song
     private void displaySongs(String playlistName) {
         boolean runOn = true;
-        int command = 0;
+        String command = null;
         Playlist playlist = new Playlist(playlistName);
 
         System.out.println("\nHere is your playlist called " + playlistName + "!");
         System.out.println("\nIt currently has " + String.valueOf(playlist.getPlaylistSize()) + " songs.");
         System.out.println(playlist.getSongList());
-        System.out.println("\nWould you like to add a song?");
-        System.out.println("\tEnter 1 for YES");
-        System.out.println("\tEnter 2 for NO");
 
         while (runOn) {
-            command = input.nextInt();
+            songAddOrRemove(playlist);
+            command = input.next();
 
-            if (command == 2) {
+            if (command.equals("2")) {
                 runOn = false;
-            } else if (command == 1) {
+            } else if (command.equals("1")) {
                 runOn = false;
-                addingSong();
+                songMenu(playlist, playlistName);
+            } else if (command.equals("3")) {
+                removeOption();
             } else {
                 System.out.println("Selection not valid.");
             }
@@ -96,8 +97,76 @@ public class PlaylistApp {
 
     }
 
-    private void addingSong() {
+    // EFFECTS: displays an option to add or remove a song to the playlist,
+    //          remove option is displayed only if the playlist is not empty
+    private void songAddOrRemove(Playlist playlist) {
+        System.out.println("\nAdd a song?");
+        System.out.println("\tEnter 1 for YES");
+        System.out.println("\tEnter 2 for NO");
 
+        if (playlist.getPlaylistSize() > 0) {
+            System.out.println("\nRemove a song?");
+            System.out.println("\tEnter 3 for YES");
+            System.out.println("\tEnter 2 for NO");
+        }
+    }
+
+    private void removeOption() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: displays options to user for making a song
+    private void songMenu(Playlist playlist, String playlistName) {
+        boolean runOn = true;
+        String command = null;
+
+        while (runOn) {
+            songOptions();
+            command = input.next();
+
+            if (command.equals("0")) {
+                runOn = false;
+                displaySongs(playlistName);
+            } else if (command.equals("1")) {
+                nameOption(playlist);
+            } else if (command.equals("2")) {
+                artistOption(playlist);
+            } else if (command.equals("3")) {
+                albumOption(playlist);
+            } else if (command.equals("4")) {
+                lengthOption(playlist);
+            } else {
+                System.out.println("\n Selection not valid.");
+            }
+        }
+
+    }
+
+    private void songOptions() {
+        System.out.println("\nOptions:");
+        System.out.println("\tEnter 1 to add the name of your song.");
+        System.out.println("\tEnter 2 to add the artist of your song.");
+        System.out.println("\tEnter 3 to add the album of your song.");
+        System.out.println("\tEnter 4 to add the length of your song.");
+        System.out.println("\tEnter 0 to view your playlist.");
+
+    }
+
+    private void nameOption(Playlist playlist) {
+
+    }
+
+    private void artistOption(Playlist playlist) {
+
+    }
+
+    private void albumOption(Playlist playlist) {
+
+    }
+
+    private void lengthOption(Playlist playlist) {
+        
     }
 
     private void sideMenu() {
